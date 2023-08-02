599c92f5e311c685505e7bca86f06b34d607a7b2
Mark Tomm <marktomm90@gmail.com>
2023-08-02 12:27:22 +0300

    move config files from kusmis project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    hmr: {
+      protocol: "ws",
+      host: "nginx",
+      clientPort: 80,
+      path: "dws",
+    },
+    // websocketServer: 'ws://node_front',
+    proxy: {
+      "/api": {
+        target: "http://node_back:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/ws": {
+        target: "http://node_back:3000",
+        changeOrigin: true,
+        ws: true,
+        secure: false,
+      },
+      "/assets": {
+        target: "http://nginx",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+}));
