2efb5365eb93c27955de4206fe7dd6e95b372c8d
Mark Tomm <marktomm90@gmail.com>
2023-08-09 15:09:46 +0300

    remove unused ws and proxy vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,30 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    hmr: {
-      protocol: "ws",
-      host: "nginx",
-      clientPort: 80,
-      path: "dws",
-    },
+    // hmr: {
+    //   protocol: "ws",
+    //   host: "nginx",
+    //   clientPort: 80,
+    //   path: "dws",
+    // },
     // websocketServer: 'ws://node_front',
-    proxy: {
-      "/api": {
-        target: "http://node_back:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/ws": {
-        target: "http://node_back:3000",
-        changeOrigin: true,
-        ws: true,
-        secure: false,
-      },
-      "/assets": {
-        target: "http://nginx",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://node_back:3000",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/ws": {
+    //     target: "http://node_back:3000",
+    //     changeOrigin: true,
+    //     ws: true,
+    //     secure: false,
+    //   },
+    //   "/assets": {
+    //     target: "http://nginx",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
 }));
